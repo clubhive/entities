956fd10dd3a0023974a625139a2566a65f3750ef
stivenVA <stivenv.ariza@gmail.com>
2024-10-25 14:23:16 -0500

    Add: IsTokenExpired method at CustomJWTParser

--- src/main/java/org/clubhive/utils/CustomJWTParser.java
+++ src/main/java/org/clubhive/utils/CustomJWTParser.java
@@ -8,6 +8,7 @@ import exceptions.NoBugsException;
 import org.springframework.http.HttpStatus;
 
 import java.text.ParseException;
+import java.util.Date;
 
 public class CustomJWTParser {
 
@@ -17,6 +18,19 @@ public class CustomJWTParser {
             JWT jwt = JWTParser.parse(token);
             JWTClaimsSet claims = jwt.getJWTClaimsSet();
             return claims.getSubject();
+        } catch (ParseException e) {
+            throw new NoBugsException("Invalid token", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+
+            JWT jwt = JWTParser.parse(token);
+            JWTClaimsSet claims = jwt.getJWTClaimsSet();
+            Date expirationTime = claims.getExpirationTime();
+
+            return expirationTime.before(new Date());
         } catch (ParseException e) {
             throw new NoBugsException("Invalid token", HttpStatus.UNAUTHORIZED);
         }
